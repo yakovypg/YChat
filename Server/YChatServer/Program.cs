3e1943a5c0f025a00e91548d6517e4a1b5c24064
yakovypg <yasha-pmp@mail.ru>
2022-11-19 01:17:59 +0300

    Add server app

---
+++ Server/YChatServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using ChatEngine.Logging;
+using ChatEngine.SeverSide;
+using System.Net;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+int port = SharedConfig.NetConfig.DEFAULT_PORT;
+
+if (args.Length == 1)
+{
+    if (!int.TryParse(Console.ReadLine(), out port))
+    {
+        Console.WriteLine("Error: incorrect port.");
+        return;
+    }
+}
+else if (args.Length > 1)
+{
+    Console.WriteLine("Error: incorrect number of arguments.");
+    return;
+}
+
+var logger = new ConsoleLogger();
+var server = new Server(IPAddress.Any, port, logger);
+
+await server.StartAsync();
