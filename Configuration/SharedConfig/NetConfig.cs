5187a790581ac1fe314080baa7c3c950bcbb4f06
yakovypg <yasha-pmp@mail.ru>
2022-11-19 01:05:23 +0300

    Create solution structure, add shared configuration project

---
+++ Configuration/SharedConfig/NetConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedConfig
+{
+    public static class NetConfig
+    {
+        public const int DEFAULT_PORT = 5000;
+        public const string DEFAULT_HOST = "127.0.0.1";
+    }
+}
